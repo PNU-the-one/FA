46b011c3a15eaa9aa8268f000e09ff169865358c
thingseong <audtjd1893@gmail.com>
2022-03-12 14:01:24 +0900

    수찾기 완료. by thingseong

---
+++ thingseong/수찾기.py
@@ -0,0 +1,34 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+nums = sorted(map(int, sys.stdin.readline().split()))
+
+
+
+M = int(sys.stdin.readline())
+
+m = map(int, sys.stdin.readline().split())
+
+
+def f(arr, m, s, e):
+    if s > e:
+        return 0
+
+    mid = (s + e) // 2
+    if arr[mid] == m:
+        return 1
+    elif arr[mid] < m:
+        return f(arr, m, mid+1, e)
+    elif arr[mid] > m:
+        return f(arr, m, 0, mid-1)
+
+
+for x in m:
+    print(f(nums, x, 0, len(nums)-1))
+    
+    
+    
+'''
+list slicing을 수행하면 시간복잡도가 상승 하는것 같다.
+'''
