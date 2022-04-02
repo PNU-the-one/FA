e9d5cbe5562aeb73f1c76b876b06eae01d32af9a
rudgns9334 <rudgns8285@naver.com>
2022-04-02 19:21:13 +0900

    최단경로(수정)

--- rudgns9334/최단 경로/dijkstra.cpp
+++ rudgns9334/최단 경로/dijkstra.cpp
@@ -9,30 +9,25 @@ using namespace std;
 int V, E, K;
 int dis[20001];
 vector<pair<int,int>> edge[20001];
-bool visited[20001];
 
 void dijkstra(int K){
     int v = K;
     int cnt = 1;
-    queue<int> q;
-    q.push(v);
-    visited[v] = true;
+    priority_queue<pair<int,int>> pq;
+    pq.push(make_pair(0,K));
     dis[v] = 0;
-    while(!q.empty()){
-        int x = q.front();
-        int low = 0;
-        q.pop();
-        for(int i=0;i<edge[x].size();i++){
-            int y = edge[x][i].first;
-            int w = edge[x][i].second;
-            if(!visited[y]){
-                if(dis[y]==0) dis[y] = w;
-                else if(dis[y] > dis[x]+w) dis[y] = dis[x]+w;
-                if(dis[low]>dis[y]) low = y;
+    while(!pq.empty()){
+        int cost = -pq.top().first;
+        int next = pq.top().second;
+        pq.pop();
+        for(int i=0;i<edge[next].size();i++){
+            int y = edge[next][i].first;
+            int w = edge[next][i].second;
+            if(dis[y] > cost+w) {
+                dis[y] = cost+w;
+                pq.push(make_pair(-dis[y],y));
             }
         }
-        q.push(low);
-        visited[low] = true;
     }
 
 }
@@ -43,14 +38,16 @@ int main()
     cin.sync_with_stdio(false);
     cin >> V >> E;
     cin >> K;
-    dis[0] = 3000001;
     for(int i=0;i<E;i++){
         int a,b,c;
         cin >> a >> b >> c;
         edge[a].push_back(make_pair(b,c));
     }
+    for(int i=1;i<=V;i++) dis[i] = 3000001;
     dijkstra(K);
     for(int i=1;i<=V;i++){
-        cout << dis[i] << "\n";
+        int d = dis[i];
+        if(dis[i]==3000001) cout << "INF" << "\n";
+        else cout << d << "\n";
     }
 }
